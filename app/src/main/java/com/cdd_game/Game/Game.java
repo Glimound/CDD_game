e340555d75615ca8397eb99c36995ba3f2e2440d
AlenYegar <202130481401@mail.scut.edu.cn>
2023-06-07 09:27:15 +0800

    增加玩家类和Game类中的set与get

--- app/src/main/java/com/cdd_game/Game/Game.java
+++ app/src/main/java/com/cdd_game/Game/Game.java
@@ -9,18 +9,37 @@ import java.util.Set;
 
 public class Game {
     private String gameID;
-    private Game gameInstance;
     private CardPool initialCards;
     private CardPool discardCards;
     private Rule rule;
     private Set<Player> players;
 
-    private static class LazyHolder {
-        private static final Game INSTANCE = new Game();
+    //懒汉式单例类.在第一次调用的时候实例化自己
+    //三个重载
+    private Game(String gameID,CardPool initialCards,CardPool discardCards,Rule rule ,Player player) {
+        this.gameID=gameID;
+        this.initialCards=initialCards;
+        this.discardCards=discardCards;
+        this.rule=rule;
+        this.players.add(player);
+
     }
-    private Game (){}
-    public static final Game getInstance() {
-            return LazyHolder.INSTANCE;
+    private Game(String gameID,CardPool initialCards,CardPool discardCards,Rule rule ,Set<Player> players) {
+        this.gameID=gameID;
+        this.initialCards=initialCards;
+        this.discardCards=discardCards;
+        this.rule=rule;
+        this.players=players;
+
+    }
+    private Game(){}
+    private static Game game=null;
+    //静态工厂方法
+    public static Game getInstance() {
+        if (game == null) {
+            game = new Game();
+        }
+        return game;
     }
 
     //TODO:
@@ -41,5 +60,41 @@ public class Game {
     public boolean removePlayer(Player player){
         players.remove(player);
         return true;
+    }
+
+    //数据成员的set和get函数
+    public void setRule(Rule rule){
+        this.rule=rule;
+    }
+    public Rule getRule(){
+        return rule;
+    }
+
+    public void setInitialCards(CardPool initialCards){
+        this.initialCards=initialCards;
+    }
+    public CardPool getInitialCards() {
+        return initialCards;
+    }
+
+    public void setDiscardCards(CardPool discardCards){
+        this.discardCards=discardCards;
+    }
+    public CardPool getDiscardCards() {
+        return discardCards;
+    }
+
+    public void setGameID(String gameID){
+        this.gameID=gameID;
+    }
+    public String getGameID() {
+        return gameID;
+    }
+
+    public void setPlayers(Set<Player> players) {
+        this.players = players;
+    }
+    public Set<Player> getPlayers() {
+        return players;
     }
 }
--- app/src/main/java/com/cdd_game/Player/Player.java
+++ app/src/main/java/com/cdd_game/Player/Player.java
@@ -1,8 +1,10 @@
 package com.cdd_game.Player;
 
 import com.cdd_game.Card.Card;
+import com.cdd_game.Card.CardGroup;
 import com.cdd_game.Card.CardPool;
 import com.cdd_game.Card.CardPoolFactory;
+import com.cdd_game.Game.Game;
 
 public class Player {
     private String nickName;
@@ -16,26 +18,24 @@ public class Player {
         this.deviceID=id;
     }
 
-
     /**
      * @param cards
      * cards 代表玩家准备出的牌
      * @return
      */
-    public boolean playCard(CardPool cards){
+    public boolean playCard(CardGroup cards){
         //TODO：首先检查出的牌是否符合规则(待修改）
-        if(Game.getGameInstance().getRule().check(cards)){
+        if(Game.getInstance().getRule().validate(cards)){
             //TODO：再检查出牌是否大于上家()
-            if(Game.getGameInstance().getRule().checkAmount(cards,Game.getGameInstance().lastcards)){
+            /*if(Game.getGameInstance().getRule().checkAmount(cards,Game.getGameInstance().lastcards)){
                 //将要出的牌从手牌中去除，并加入到弃牌堆中
                 ownCards.removeCards(cards);
                 Game.getGameInstance().getDiscardCards().addCards(cards);
+                */
                 return true;
             }
-        }
         return false;
-    }
-
+}
     public boolean pass(){
         //TODO:
         return true;
@@ -45,9 +45,30 @@ public class Player {
      * TODO:传入参数类型
      * @return
      */
-    public boolean sortCard(int para){
-        this.ownCards.sort(para);
+    public boolean sortCard(){
+        this.ownCards.sort();
         return true;
     }
 
+    //数据成员的set与get
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+    public String getNickName(){
+        return this.nickName;
+    }
+    public void setOwnCards(CardPool ownCards) {
+        this.ownCards = ownCards;
+    }
+
+    public CardPool getOwnsCards() {
+        return this.ownCards;
+    }
+
+    public void setDeviceID(String deviceID) {
+        this.deviceID = deviceID;
+    }
+    public String getDeviceID(){
+        return this.deviceID;
+    }
 }
