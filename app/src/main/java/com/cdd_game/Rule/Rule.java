6444b66cfaa5052c6423cb1e20c7ae78b5675307
Glimound <2633956565@qq.com>
2023-06-11 17:29:55 +0800

    新增NormalRule中计算玩家得分的测试；修复bug

--- app/src/main/java/com/cdd_game/Rule/Rule.java
+++ app/src/main/java/com/cdd_game/Rule/Rule.java
@@ -1,5 +1,6 @@
 package com.cdd_game.Rule;
 import com.cdd_game.Card.*;
+import com.cdd_game.Player.Player;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -7,5 +8,6 @@ import java.util.HashMap;
 // TODO: 实现策略模式，拼接游戏规则：几人局、校验规则等待
 public interface Rule {
     abstract boolean validate(CardGroup cards);
+    abstract HashMap<Player,Integer> computeGameScore(HashMap<Player, CardPool> remainingCards);
 
 }
